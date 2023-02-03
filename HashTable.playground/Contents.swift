8fa53a0791b85a99fc7ead76d26bbc18b9ad91ec
ravendeng <dlfkid@icloud.com>
2023-02-03 15:42:42 +0800

    feat: addd hashTale question 349 solution

--- HashTable.playground/Contents.swift
+++ HashTable.playground/Contents.swift
@@ -49,6 +49,36 @@ class HashTableSolution {
         }
         return result
     }
-}
+    
+    /* No.349
+     给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
 
-HashTableSolution().isAnagram("anagram", "nagaram")
+      
+
+     示例 1：
+
+     输入：nums1 = [1,2,2,1], nums2 = [2,2]
+     输出：[2]
+     示例 2：
+
+     输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+     输出：[9,4]
+     解释：[4,9] 也是可通过的
+      
+
+     提示：
+
+     1 <= nums1.length, nums2.length <= 1000
+     0 <= nums1[i], nums2[i] <= 1000
+     */
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var nums1Set = Set(nums1)
+        var resultSet = Set<Int>()
+        for number in nums2 {
+            if nums1Set.contains(number) {
+                resultSet.insert(number)
+            }
+        }
+        return Array(resultSet)
+    }
+}
