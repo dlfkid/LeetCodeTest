6a652b8cc016d0b20c12752e78103bdbf65045b5
ravendeng <dlfkid@icloud.com>
2023-02-07 14:30:16 +0800

    feat: add 三数之和 solution

--- HashTable.playground/Contents.swift
+++ HashTable.playground/Contents.swift
@@ -227,9 +227,114 @@ class HashTableSolution {
         }
         return result
     }
+    
+    /* No.15 三数之和
+     给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+     你返回所有和为 0 且不重复的三元组。
+
+     注意：答案中不可以包含重复的三元组。
+
+      
+
+      
+
+     示例 1：
+
+     输入：nums = [-1,0,1,2,-1,-4]
+     输出：[[-1,-1,2],[-1,0,1]]
+     解释：
+     nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+     nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+     nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+     不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+     注意，输出的顺序和三元组的顺序并不重要。
+     示例 2：
+
+     输入：nums = [0,1,1]
+     输出：[]
+     解释：唯一可能的三元组和不为 0 。
+     示例 3：
+
+     输入：nums = [0,0,0]
+     输出：[[0,0,0]]
+     解释：唯一可能的三元组和为 0 。
+      
+
+     提示：
+
+     3 <= nums.length <= 3000
+     -105 <= nums[i] <= 105
+     
+    */
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        let sortedNums = nums.sorted()
+        for baseIndex in 0 ..< sortedNums.count {
+            guard sortedNums[baseIndex] <= 0 else {
+                return result
+            }
+            if baseIndex > 0 && sortedNums[baseIndex - 1] == sortedNums[baseIndex] {
+                continue
+            }
+            var left = baseIndex + 1
+            var right = sortedNums.count - 1
+            while left < right  {
+                let sum = sortedNums[baseIndex] + sortedNums[left] + sortedNums[right]
+                print("base: \(sortedNums[baseIndex]) left: \(sortedNums[left]) right: \(sortedNums[right]) sum: \(sum)")
+                if sum > 0 {
+                    right -= 1
+                } else if sum < 0 {
+                    left += 1
+                } else {
+                    result.append([sortedNums[left], sortedNums[right], sortedNums[baseIndex]])
+                    while right > left && sortedNums[left + 1] == sortedNums[left] {
+                        left += 1
+                    }
+                    while right > left && sortedNums[right] == sortedNums[right - 1] {
+                        right -= 1
+                    }
+                    left += 1
+                    right -= 1
+                }
+            }
+        }
+        return result
+    }
 }
 
-HashTableSolution().fourSumCount([1,2],
-                                  [-2,-1],
-                                  [-1,2],
-                                  [0,2])
+extension Array where Element == Int {
+    func quickSortArray() -> [Int] {
+        var result = Array(self)
+        quickSortInternal(input: &result, left: 0, right: self.count - 1)
+        return result
+    }
+    
+    func quickSortInternal(input: inout [Int], left: Int, right: Int) {
+        guard right > left else {
+            return
+        }
+        var tempLeft = left
+        var tempRight = right
+        let baseValue = input[left]
+        while tempRight > tempLeft {
+            while tempRight > tempLeft && input[tempRight] >= baseValue {
+                tempRight -= 1
+            }
+            if tempRight > tempLeft {
+                input[tempLeft] = input[tempRight]
+            }
+            while tempRight > tempLeft && input[tempLeft] < baseValue {
+                tempLeft += 1
+            }
+            if tempRight > tempLeft {
+                input[tempRight] = input[tempLeft]
+            }
+        }
+        let divide = tempLeft
+        quickSortInternal(input: &input, left: left, right: divide - 1)
+        quickSortInternal(input: &input, left: divide + 1, right: right)
+    }
+}
+
+HashTableSolution().threeSum([0,0,0])
