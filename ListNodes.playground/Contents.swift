8bf3d63a2908adde73e1554e1df37caebf40ddf1
ravendeng <dlfkid@icloud.com>
2023-02-19 23:37:58 +0800

    add: 两两交换链表的节点

--- ListNodes.playground/Contents.swift
+++ ListNodes.playground/Contents.swift
@@ -190,4 +190,37 @@ class ListNodesSolutions {
         let newCurrent = nextNode
         return reversTwoNode(newCurrent, newPre)
     }
+    
+    /*
+     给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+
+     输入：head = [1,2,3,4]
+     输出：[2,1,4,3]
+     示例 2：
+
+     输入：head = []
+     输出：[]
+     示例 3：
+
+     输入：head = [1]
+     输出：[1]
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/swap-nodes-in-pairs
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        let dummyHead = ListNode(-1, head)
+        var currnt: ListNode? = dummyHead
+        while currnt?.next != nil && currnt?.next?.next != nil {
+            let node1 = currnt?.next
+            let node2 = currnt?.next?.next
+            let node3 = node2?.next
+            currnt?.next = node2
+            node2?.next = node1
+            node1?.next = node3
+            currnt = currnt?.next?.next
+        }
+        return dummyHead.next
+    }
 }
