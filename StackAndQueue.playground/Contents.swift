198798d64127ea06f322d0ce011554d466abd4f9
ravendeng <dlfkid@icloud.com>
2023-03-17 15:20:10 +0800

    add: 逆波兰表达式算法题解

--- StackAndQueue.playground/Contents.swift
+++ StackAndQueue.playground/Contents.swift
@@ -1,5 +1,15 @@
 import Cocoa
 
+extension String {
+    var isCalculateSymbol: Bool {
+        guard self.count == 1 else {
+            return false
+        }
+        let mapping = ["+": 1, "-": 1, "*": 1, "/": 1]
+        return mapping[self] != nil
+    }
+}
+
 /*
  请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
 
@@ -162,4 +172,118 @@ class StackAndQueueSolution {
         // 如果遍历完了栈还没有空，说明左括号太多了， 返回错误
         return charStack.isEmpty
     }
+    
+    /*
+     给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+
+     在 S 上反复执行重复项删除操作，直到无法继续删除。
+
+     在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+
+      
+
+     示例：
+
+     输入："abbaca"
+     输出："ca"
+     解释：
+     例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/remove-all-adjacent-duplicates-in-string
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    
+    func removeDuplicates(_ s: String) -> String {
+        var stack = [Character]()
+        for char in s {
+            if stack.isEmpty {
+                stack.append(char)
+                continue
+            }
+            let stackChar = stack.popLast()
+            if stackChar != char {
+                stack.append(stackChar!)
+                stack.append(char)
+            }
+        }
+        return String(stack)
+    }
+    
+    /*
+     给你一个字符串数组 tokens ，表示一个根据 逆波兰表示法 表示的算术表达式。
+
+     请你计算该表达式。返回一个表示表达式值的整数。
+
+     注意：
+
+     有效的算符为 '+'、'-'、'*' 和 '/' 。
+     每个操作数（运算对象）都可以是一个整数或者另一个表达式。
+     两个整数之间的除法总是 向零截断 。
+     表达式中不含除零运算。
+     输入是一个根据逆波兰表示法表示的算术表达式。
+     答案及所有中间计算结果可以用 32 位 整数表示。
+      
+
+     示例 1：
+
+     输入：tokens = ["2","1","+","3","*"]
+     输出：9
+     解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+     示例 2：
+
+     输入：tokens = ["4","13","5","/","+"]
+     输出：6
+     解释：该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
+     示例 3：
+
+     输入：tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+     输出：22
+     解释：该算式转化为常见的中缀算术表达式为：
+       ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+     = ((10 * (6 / (12 * -11))) + 17) + 5
+     = ((10 * (6 / -132)) + 17) + 5
+     = ((10 * 0) + 17) + 5
+     = (0 + 17) + 5
+     = 17 + 5
+     = 22
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/evaluate-reverse-polish-notation
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    
+    func evalRPN(_ tokens: [String]) -> Int {
+        var stack = [Int]()
+        for symbol in tokens {
+            // 遇到非运算符就直接入栈
+            if !symbol.isCalculateSymbol {
+                print("pushed \(symbol)")
+                stack.append(Int(symbol) ?? 0)
+                continue
+            }
+            // 遇到运算符，则从栈中取出两个元素计算，并把结果放回栈内
+            if stack.count > 1 {
+                guard let x = stack.popLast(), let y = stack.popLast() else {
+                    continue
+                }
+                print("calculating \(x) \(symbol) \(y)")
+                var result = 0
+                if symbol == "+" {
+                    result = y + x
+                } else if symbol == "-" {
+                    result = y - x
+                } else if symbol == "*" {
+                    result = y * x
+                } else  {
+                    result = y / x
+                }
+                print("pushed temp result \(result)")
+                stack.append(result)
+            }
+        }
+        return stack.popLast() ?? 0
+    }
 }
+
+StackAndQueueSolution().evalRPN(["4","13","5","/","+"])
