19cef727238b9247b05acc0fd388f02f4b71e188
ravendeng <dlfkid@icloud.com>
2023-03-31 13:43:11 +0800

    add: 二叉搜索树插入子节点

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -998,6 +998,49 @@ class TreeNodeSolution {
         }
         return result
     }
+    
+    /*
+     给定二叉搜索树（BST）的根节点 root 和要插入树中的值 value ，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+
+     注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+
+      
+
+     示例 1：
+
+
+     输入：root = [4,2,7,1,3], val = 5
+     输出：[4,2,7,1,3,5]
+     解释：另一个满足题目要求可以通过的树是：
+
+     示例 2：
+
+     输入：root = [40,20,60,10,30,50,70], val = 25
+     输出：[40,20,60,10,30,50,70,null,null,25]
+     示例 3：
+
+     输入：root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
+     输出：[4,2,7,1,3,5]
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/insert-into-a-binary-search-tree
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        // 二叉搜索树插入节点，可以插入在叶子节点上，因此终止条件应该是遇到叶子节点
+        guard let root = root else {
+            return TreeNode(val)
+        }
+        // 节点值大于目标值，说明该向左遍历，要插入的节点在左子结点
+        if root.val > val {
+            root.left = insertIntoBST(root.left, val)
+        }
+        // 节点值小于目标值，说明该向右遍历，要插入的节点在右子结点
+        if root.val < val {
+            root.right = insertIntoBST(root.right, val)
+        }
+        return root
+    }
 }
 
 public class Node {
