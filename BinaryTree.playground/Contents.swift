09c2cab66b007e5dfc24f0eaa283ac306c92a8ab
ravendeng <dlfkid@icloud.com>
2023-04-02 13:47:26 +0800

    add: 增加修建二叉树算法题解

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -1110,6 +1110,48 @@ class TreeNodeSolution {
         }
         return root
     }
+    
+    /*
+     给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树 不应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。 可以证明，存在 唯一的答案 。
+
+     所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
+
+      
+
+     示例 1：
+
+
+     输入：root = [1,0,2], low = 1, high = 2
+     输出：[1,null,2]
+     示例 2：
+
+
+     输入：root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+     输出：[3,2,null,1]
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/trim-a-binary-search-tree
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func trimBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+        // 判断当前节点值是否在范围内，如果小于范围，递归检查右子树，因为二叉搜索树的右子树节点值比根节点值大， 有可能符合范围
+        if root.val < low {
+            root.left = trimBST(root.right, low, high)
+            return root
+        }
+        // 判断当前节点值是否在范围内，如果大于范围，递归检查左子树，因为二叉搜索树的左子树节点值比根节点值大，有可能符合范围
+        if root.val > high {
+            root.right = trimBST(root.left, low, high)
+            return root
+        }
+        // 对于符合要求的那些节点，递归继续检查即可
+        root.left = trimBST(root.left, low, high)
+        root.right = trimBST(root.right, low, high)
+        return root
+    }
 }
 
 public class Node {
