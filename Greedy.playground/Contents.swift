a4e01975bdc1be0b9044e9b67cf9c7e79b54817f
ravendeng <dlfkid@icloud.com>
2023-04-10 10:39:43 +0800

    add: 补充贪心算法题解

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -241,6 +241,50 @@ class GreedySolutions {
         }
         return profix
     }
+    
+    /*
+     给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+
+     数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+     判断你是否能够到达最后一个下标。
+
+      
+
+     示例 1：
+
+     输入：nums = [2,3,1,1,4]
+     输出：true
+     解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+     示例 2：
+
+     输入：nums = [3,2,1,0,4]
+     输出：false
+     解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+      
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/jump-game
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func canJump(_ nums: [Int]) -> Bool {
+        // 使用贪心算法，不纠结具体的跳法，只需要计算在每个位置上的最远跳跃
+        if nums.count <= 1 {
+            return true
+        }
+        var cover = nums[0]
+        var index = 0
+        var result = false
+        while index <= cover {
+            cover = max(index + nums[index], cover)
+            if cover >= nums.count - 1 {
+                result = true
+                break
+            }
+            index += 1
+        }
+        return result
+    }
 }
 
-GreedySolutions().minNumberOfHours(5, 3, [1, 4, 3, 2], [2, 6, 3, 1])
+GreedySolutions().canJump([3,2,1,0,4])
