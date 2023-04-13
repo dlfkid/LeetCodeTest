43c8c3dca59271f06594694ef5c9506ca2964ba5
ravendeng <dlfkid@icloud.com>
2023-04-13 19:59:24 +0800

    add: 增加贪心算法，k次取反求最大数组和

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -346,6 +346,61 @@ class GreedySolutions {
         }
         return result
     }
+    
+    /*
+     给你一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
+
+     选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
+     重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
+
+     以这种方式修改数组后，返回数组 可能的最大和 。
+
+      
+
+     示例 1：
+
+     输入：nums = [4,2,3], k = 1
+     输出：5
+     解释：选择下标 1 ，nums 变为 [4,-2,3] 。
+     示例 2：
+
+     输入：nums = [3,-1,0,2], k = 3
+     输出：6
+     解释：选择下标 (1, 2, 2) ，nums 变为 [3,1,0,2] 。
+     示例 3：
+
+     输入：nums = [2,-3,-1,5,-4], k = 2
+     输出：13
+     解释：选择下标 (1, 4) ，nums 变为 [2,3,-1,5,4] 。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/maximize-sum-of-array-after-k-negations
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func largestSumAfterKNegations(_ nums: [Int], _ k: Int) -> Int {
+        // 首先按照绝对值从大到小的顺序给数组排序
+        var sortedNums = nums.sorted { a, b in
+            return abs(a) > abs(b)
+        }
+        var leftChance = k
+        // 第一次贪心，当遍历到小于0的数组，对它取反
+        for index in 0 ..< sortedNums.count {
+            if sortedNums[index] < 0 && leftChance > 0 {
+                // 取反
+                sortedNums[index] *= -1
+                leftChance -= 1
+            }
+        }
+        // 如果此时k没有消耗完，进行第二次贪心
+        // 如果k是奇数，直接对最后一位取反，因为如果剩余偶数次的操作的话可以反复对一位数取反使得原数组不变
+        if leftChance % 2 == 1 {
+            sortedNums[sortedNums.count - 1] *= -1
+        }
+        // 输出数组和
+        return sortedNums.reduce(0, {
+            $0 + $1
+        })
+    }
 }
 
-GreedySolutions().jump([2,3,1,1,4])
+GreedySolutions().largestSumAfterKNegations([3,-1,0,2], 6)
