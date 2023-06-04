4ce410367ca7a66fc1d9eeca4055eba8d6898e48
RavenDeng <dlfkid@icloud.com>
2023-06-04 15:58:40 +0800

    add: 增加贪心算法，柠檬水找零问题

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -521,7 +521,73 @@ class GreedySolutions {
         }
         return candyDistribute.reduce(0, +)
     }
+    
+    /*
+     在柠檬水摊上，每一杯柠檬水的售价为 5 美元。顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+
+     每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+
+     注意，一开始你手头没有任何零钱。
+
+     给你一个整数数组 bills ，其中 bills[i] 是第 i 位顾客付的账。如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+
+      
+
+     示例 1：
+
+     输入：bills = [5,5,5,10,20]
+     输出：true
+     解释：
+     前 3 位顾客那里，我们按顺序收取 3 张 5 美元的钞票。
+     第 4 位顾客那里，我们收取一张 10 美元的钞票，并返还 5 美元。
+     第 5 位顾客那里，我们找还一张 10 美元的钞票和一张 5 美元的钞票。
+     由于所有客户都得到了正确的找零，所以我们输出 true。
+     示例 2：
+
+     输入：bills = [5,5,10,10,20]
+     输出：false
+     解释：
+     前 2 位顾客那里，我们按顺序收取 2 张 5 美元的钞票。
+     对于接下来的 2 位顾客，我们收取一张 10 美元的钞票，然后返还 5 美元。
+     对于最后一位顾客，我们无法退回 15 美元，因为我们现在只有两张 10 美元的钞票。
+     由于不是每位顾客都得到了正确的找零，所以答案是 false。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/lemonade-change
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func lemonadeChange(_ bills: [Int]) -> Bool {
+        var result = true
+        // 用三个变量储存每种面值纸币的张数
+        var fiveBucks = 0, tenBucks = 0, twentyBucks = 0
+        for bill in bills {
+            // 针对客户每次付账的面值，计算每种面值纸币的剩余张数
+            if bill == 5 {
+                // 5刀的话不用找零， 直接加1
+                fiveBucks += 1
+            } else if bill == 10 {
+                // 10刀的话要找零一张5刀，10刀+1，5刀-1
+                tenBucks += 1
+                fiveBucks -= 1
+            } else if bill == 20 {
+                // 如果是20刀， 优先用10刀+5刀的组合去找零，没有的话用三张5刀来找零。 这里局部最优是尽可能保留通用性更好的5刀，全局最优是对每个客户都能找零。
+                twentyBucks += 1
+                if tenBucks > 0 {
+                    tenBucks -= 1
+                    fiveBucks -= 1
+                } else {
+                    fiveBucks -= 3
+                }
+            }
+            // 每次结账后检查库存， 如果有纸币的面值是负数，说明初始不带零钱是找不开的。
+            if fiveBucks < 0 || tenBucks < 0 {
+                result = false
+                break
+            }
+        }
+        return result
+    }
 }
 
-GreedySolutions().candy([1,2,2])
+GreedySolutions().lemonadeChange([5,5,10,10,20])
 
