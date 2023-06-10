434ef7fe2c3ce3d11b7dfaa1ff682988d942a72f
RavenDeng <dlfkid@icloud.com>
2023-06-10 15:51:07 +0800

    add: 增加贪心算法，身高排队问题

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -587,7 +587,61 @@ class GreedySolutions {
         }
         return result
     }
+    
+    /*
+     假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+
+     请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）。
+
+      
+
+     示例 1：
+
+     输入：people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+     输出：[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+     解释：
+     编号为 0 的人身高为 5 ，没有身高更高或者相同的人排在他前面。
+     编号为 1 的人身高为 7 ，没有身高更高或者相同的人排在他前面。
+     编号为 2 的人身高为 5 ，有 2 个身高更高或者相同的人排在他前面，即编号为 0 和 1 的人。
+     编号为 3 的人身高为 6 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+     编号为 4 的人身高为 4 ，有 4 个身高更高或者相同的人排在他前面，即编号为 0、1、2、3 的人。
+     编号为 5 的人身高为 7 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+     因此 [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] 是重新构造后的队列。
+     示例 2：
+
+     输入：people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+     输出：[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/queue-reconstruction-by-height
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+        typealias PeopleInfo = (height: Int, higherThanOrEqualto: Int)
+        // 入参是二维数组，第一个值是身高，第二个值是排在自己前面比自己高的人，因此我们声明一个元组来存放排队信息
+        var sortedInfos = [PeopleInfo]()
+        for info: [Int] in people {
+            let peopleInfo: PeopleInfo = (info[0], info[1])
+            sortedInfos.append(peopleInfo)
+        }
+        // 对所有人进行排序，身高从高到低排，相同身高时前面人数越少的排前面。
+        sortedInfos.sort { (info1, info2) -> Bool in
+            if info1.height == info2.height {
+                return info1.higherThanOrEqualto < info2.higherThanOrEqualto
+            }
+            return info1.height > info2.height
+        }
+        // 构造结果数组
+        var result = [[Int]]()
+        for (_, targetPeople) in sortedInfos.enumerated() {
+            // 把每个人按照他前面的人数定为插入位置，插入到队列中
+            let position = targetPeople.higherThanOrEqualto;
+            result.insert([targetPeople.height, targetPeople.higherThanOrEqualto], at: result.startIndex + position)
+        }
+        // 输出结果队列
+        return result
+    }
 }
 
-GreedySolutions().lemonadeChange([5,5,10,10,20])
+GreedySolutions().reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]])
 
