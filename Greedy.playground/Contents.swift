289bef404c3fdd3740cb9cf64e2d6499b2bce838
RavenDeng <dlfkid@icloud.com>
2023-06-16 23:08:50 +0800

    add: 增加贪心算法，气球射箭问题

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -641,6 +641,59 @@ class GreedySolutions {
         // 输出结果队列
         return result
     }
+    
+    /*
+     有一些球形气球贴在一堵用 XY 平面表示的墙面上。墙面上的气球记录在整数数组 points ，其中points[i] = [xstart, xend] 表示水平直径在 xstart 和 xend之间的气球。你不知道气球的确切 y 坐标。
+
+     一支弓箭可以沿着 x 轴从不同点 完全垂直 地射出。在坐标 x 处射出一支箭，若有一个气球的直径的开始和结束坐标为 xstart，xend， 且满足  xstart ≤ x ≤ xend，则该气球会被 引爆 。可以射出的弓箭的数量 没有限制 。 弓箭一旦被射出之后，可以无限地前进。
+
+     给你一个数组 points ，返回引爆所有气球所必须射出的 最小 弓箭数 。
+
+      
+     示例 1：
+
+     输入：points = [[10,16],[2,8],[1,6],[7,12]]
+     输出：2
+     解释：气球可以用2支箭来爆破:
+     -在x = 6处射出箭，击破气球[2,8]和[1,6]。
+     -在x = 11处发射箭，击破气球[10,16]和[7,12]。
+     示例 2：
+
+     输入：points = [[1,2],[3,4],[5,6],[7,8]]
+     输出：4
+     解释：每个气球需要射出一支箭，总共需要4支箭。
+     示例 3：
+
+     输入：points = [[1,2],[2,3],[3,4],[4,5]]
+     输出：2
+     解释：气球可以用2支箭来爆破:
+     - 在x = 2处发射箭，击破气球[1,2]和[2,3]。
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func findMinArrowShots(_ points: [[Int]]) -> Int {
+        if points.count == 0 {
+            return 0
+        }
+        // 按照气球的结束位置排序
+        let sortedPoints = points.sorted { (point1, point2) -> Bool in
+            return point1[0] < point2[0]
+        }
+        // 记录射箭的次数
+        var count = 1
+        // 记录射箭的位置
+        var position = sortedPoints[0][1]
+        for point in sortedPoints {
+            // 如果气球的开始位置大于射箭的位置，说明这支箭不能射爆这个气球，需要再射一支箭
+            if point[0] > position {
+                position = point[1]
+                count += 1
+            }
+        }
+        return count
+    }
 }
 
 GreedySolutions().reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]])
