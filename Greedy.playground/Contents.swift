69e617358c0889b832fe8aa1acd7cc0f83f629d5
RavenDeng <dlfkid@icloud.com>
2023-07-01 15:10:55 +0800

    add: 增加贪心算法，删除重叠区间问题

--- Greedy.playground/Contents.swift
+++ Greedy.playground/Contents.swift
@@ -694,7 +694,67 @@ class GreedySolutions {
         }
         return count
     }
+    
+    /*
+     给定一个区间的集合 intervals ，其中 intervals[i] = [starti, endi] 。返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+
+      
+
+     示例 1:
+
+     输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+     输出: 1
+     解释: 移除 [1,3] 后，剩下的区间没有重叠。
+     示例 2:
+
+     输入: intervals = [ [1,2], [1,2], [1,2] ]
+     输出: 2
+     解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+     示例 3:
+
+     输入: intervals = [ [1,2], [2,3] ]
+     输出: 0
+     解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+      
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode.cn/problems/non-overlapping-intervals
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
+        // 一个区间以下的直接不用排序了
+        if intervals.count <= 1 {
+            return 0
+        }
+        // 核心思路是先对所有区间进行排序, 然后删除那些有重叠的区间.
+        var sortedIntervals = intervals.sorted {
+            // 对区间进行左边界排序
+            $0[0] < $1[0]
+        }
+        var result = 0;
+        // 遍历排序后的区间
+        for index in 0 ..< sortedIntervals.count {
+            // 第一个区间不用遍历
+            if (index == 0) {
+                continue
+            }
+            // 当前区间
+            var currentInterval = sortedIntervals[index]
+            // 上一个区间
+            let lastInterval = sortedIntervals[index - 1]
+            // 如果上一个区间的右边界大于当前区间的左边界, 判定为重叠
+            if lastInterval[1] > currentInterval[0] {
+                // 计数增加
+                result += 1
+                // 修正当前区间的右边界, 取当前区间和上一个区间右边界的最小值, 相当于两个区间只保留一个, 再和下一个区间做重叠比较
+                currentInterval[1] = min(currentInterval[1], lastInterval[1])
+                // 赋值回去
+                sortedIntervals[index] = currentInterval
+            }
+        }
+        return result
+    }
 }
 
-GreedySolutions().reconstructQueue([[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]])
+GreedySolutions().eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]])
 
