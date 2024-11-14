cf9aabe4d3026bc68e83a5827bd34790be2d4ea5
RavenDeng <dlfkid@icloud.com>
2024-11-14 11:26:14 +0800

    add: [哈希表] 219. 存在重复元素 II

--- HashTable.playground/Contents.swift
+++ HashTable.playground/Contents.swift
@@ -559,6 +559,44 @@ class HashTableSolution {
         }
         return sum
     }
+    
+    /*
+     219. 存在重复元素II
+     给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+
+      
+
+     示例 1：
+
+     输入：nums = [1,2,3,1], k = 3
+     输出：true
+     示例 2：
+
+     输入：nums = [1,0,1,1], k = 1
+     输出：true
+     示例 3：
+
+     输入：nums = [1,2,3,1,2,3], k = 2
+     输出：false
+     */
+    func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
+        var hashTable = [Int: Int]()
+        var result = false
+        for index in 0 ..< nums.count {
+            let val = nums[index]
+            if let preIndex = hashTable[val] {
+                result = abs(index - preIndex) <= k
+                if result {
+                    break
+                } else {
+                    hashTable[val] = index
+                }
+            } else {
+                hashTable[val] = index
+            }
+        }
+        return result
+    }
 }
 
-HashTableSolution().dietPlanPerformance([6, 5, 0, 0], 2, 1, 5)
+HashTableSolution().containsNearbyDuplicate([1,0,1,1], 1)
