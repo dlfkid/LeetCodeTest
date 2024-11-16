6d77ff1b1b5566bb3b03c1ea0c717f267b80af91
RavenDeng <dlfkid@icloud.com>
2024-11-16 18:25:45 +0800

    add: [字符串] 判断是否是子序列

--- StringSolution.playground/Contents.swift
+++ StringSolution.playground/Contents.swift
@@ -303,7 +303,56 @@ class StringSolution {
         }
         return String(aNums)
     }
-}
-StringSolution().reverseWords("a good   example")
+    
+    /*
+     392. 判断子序列
+     给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
 
-"aabaaf".kmpNext
+     字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+     进阶：
+
+     如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+
+     致谢：
+
+     特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+
+      
+
+     示例 1：
+
+     输入：s = "abc", t = "ahbgdc"
+     输出：true
+     示例 2：
+
+     输入：s = "axc", t = "ahbgdc"
+     输出：false
+     */
+    func isSubsequence(_ s: String, _ t: String) -> Bool {
+        guard s.count <= t.count else {
+            return false
+        }
+        guard s.count > 0 else {
+            return false
+        }
+        if s == t {
+            return true
+        }
+        var result = false
+        var slow = 0
+        var fast = 0
+        let charaArray = Array(t)
+        let subArray = Array(s)
+        while fast < charaArray.count, slow < subArray.count {
+            let tempChara = charaArray[fast]
+            if tempChara == subArray[slow] {
+                slow += 1
+            }
+            fast += 1
+        }
+        result = slow == subArray.count
+        return result
+    }
+}
+StringSolution().isSubsequence("b", "abc")
