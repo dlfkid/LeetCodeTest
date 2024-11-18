29f0029a93c64af7be35d0596478a2ed99fb1907
RavenDeng <dlfkid@icloud.com>
2024-11-18 11:46:40 +0800

    add: [数组] 661. 图片平滑器

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -505,5 +505,98 @@ class ArraySolution {
         }
         return result
     }
+    
+    /*
+     661. 图片平滑器
+     图像平滑器 是大小为 3 x 3 的过滤器，用于对图像的每个单元格平滑处理，平滑处理后单元格的值为该单元格的平均灰度。
+
+     每个单元格的  平均灰度 定义为：该单元格自身及其周围的 8 个单元格的平均值，结果需向下取整。（即，需要计算蓝色平滑器中 9 个单元格的平均值）。
+
+     如果一个单元格周围存在单元格缺失的情况，则计算平均灰度时不考虑缺失的单元格（即，需要计算红色平滑器中 4 个单元格的平均值）。
+     给你一个表示图像灰度的 m x n 整数矩阵 img ，返回对图像的每个单元格平滑处理后的图像 。
+     示例 1:
+     输入:img = [[1,1,1],[1,0,1],[1,1,1]]
+     输出:[[0, 0, 0],[0, 0, 0], [0, 0, 0]]
+     解释:
+     对于点 (0,0), (0,2), (2,0), (2,2): 平均(3/4) = 平均(0.75) = 0
+     对于点 (0,1), (1,0), (1,2), (2,1): 平均(5/6) = 平均(0.83333333) = 0
+     对于点 (1,1): 平均(8/9) = 平均(0.88888889) = 0
+     示例 2:
+
+
+     输入: img = [[100,200,100],[200,50,200],[100,200,100]]
+     输出: [[137,141,137],[141,138,141],[137,141,137]]
+     解释:
+     对于点 (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+     对于点 (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+     对于点 (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+     */
+    func imageSmoother(_ img: [[Int]]) -> [[Int]] {
+        guard img.count > 0, let firstLine = img.first, firstLine.count > 0 else {
+            return img
+        }
+        let line = img.count
+        let column = firstLine.count
+        if column == 1, line == 1 {
+            return img
+        }
+        let lineCount = img.count
+        let columnCount = firstLine.count
+        var result = Array(repeating: Array(repeating: 0, count: columnCount), count: lineCount)
+        for lineIndex in 0 ..< lineCount {
+            for columnIndex in 0 ..< columnCount {
+                // 需要被求平均值的数字
+                let gridNum = img[lineIndex][columnIndex]
+                var sum = gridNum
+                var totalCount = 1
+                // 求周围一圈数字和自己的平均数
+                if lineIndex - 1 >= 0, columnIndex - 1 >= 0 {
+                    let topLeft = img[lineIndex - 1][columnIndex - 1]
+                    sum += topLeft
+                    totalCount += 1
+                }
+                if lineIndex - 1 >= 0 {
+                    let top = img[lineIndex - 1][columnIndex]
+                    sum += top
+                    totalCount += 1
+                }
+                if lineIndex - 1 >= 0, columnIndex + 1 < columnCount {
+                    let topRight = img[lineIndex - 1][columnIndex + 1]
+                    sum += topRight
+                    totalCount += 1
+                }
+                if columnIndex - 1 >= 0 {
+                    let left = img[lineIndex][columnIndex - 1]
+                    sum += left
+                    totalCount += 1
+                }
+                if columnIndex + 1 < columnCount {
+                    let right = img[lineIndex][columnIndex + 1]
+                    sum += right
+                    totalCount += 1
+                }
+                if columnIndex - 1 >= 0, lineIndex + 1 < lineCount {
+                    let bottomLeft = img[lineIndex + 1][columnIndex - 1]
+                    sum += bottomLeft
+                    totalCount += 1
+                }
+                if lineIndex + 1 < lineCount {
+                    let bottom = img[lineIndex + 1][columnIndex]
+                    sum += bottom
+                    totalCount += 1
+                }
+                if columnIndex + 1 < columnCount, lineIndex + 1 < lineCount {
+                    let bottomRight = img[lineIndex + 1][columnIndex + 1]
+                    sum += bottomRight
+                    totalCount += 1
+                }
+                let avg = sum / totalCount
+                if columnIndex == 1, lineIndex == 1 {
+                    print("center sunm = \(sum), count = \(totalCount), avg = \(avg)")
+                }
+                result[lineIndex][columnIndex] = avg
+            }
+        }
+        return result
+    }
 }
-ArraySolution().searchInsert([1,3,5,6], 7)
