44df37c722f347e08af8266b4b88f2cf41b884a3
RavenDeng <dlfkid@icloud.com>
2024-11-22 15:36:50 +0800

    add: [数组] 11. 盛水最多的容器

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -685,5 +685,54 @@ class ArraySolution {
         }
         return result
     }
+    
+    /*
+     
+     11. 盛最多水的容器
+     给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+
+     找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+     返回容器可以储存的最大水量。
+
+     说明：你不能倾斜容器。
+
+      
+
+     示例 1：
+
+
+
+     输入：[1,8,6,2,5,4,8,3,7]
+     输出：49
+     解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+     示例 2：
+
+     输入：height = [1,1]
+     输出：1
+      
+     */
+    func maxArea(_ height: [Int]) -> Int {
+        guard height.count > 1 else {
+            return 0
+        }
+        var left = 0
+        var right = height.count - 1
+        var result = 0
+        while left < right {
+            let val1 = height[left]
+            let val2 = height[right]
+            let volume = min(val1, val2) * (right - left)
+            if volume > result {
+                result = volume
+            }
+            if val1 > val2 {
+                right -= 1
+            } else {
+                left += 1
+            }
+        }
+        return result
+    }
 }
-ArraySolution().productExceptSelf([1,2,3,4])
+ArraySolution().maxArea([1,8,6,2,5,4,8,3,7])
