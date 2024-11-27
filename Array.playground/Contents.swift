e0a3e931dc704a97eacecc5478b979f47b731f78
RavenDeng <dlfkid@icloud.com>
2024-11-27 15:14:05 +0800

    add: [数组] 228. 汇总区间

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -734,5 +734,66 @@ class ArraySolution {
         }
         return result
     }
+    
+    /*
+     228. 汇总区间
+     给定一个  无重复元素 的 有序 整数数组 nums 。
+
+     返回 恰好覆盖数组中所有数字 的 最小有序 区间范围列表 。也就是说，nums 的每个元素都恰好被某个区间范围所覆盖，并且不存在属于某个范围但不属于 nums 的数字 x 。
+
+     列表中的每个区间范围 [a,b] 应该按如下格式输出：
+
+     "a->b" ，如果 a != b
+     "a" ，如果 a == b
+      
+
+     示例 1：
+
+     输入：nums = [0,1,2,4,5,7]
+     输出：["0->2","4->5","7"]
+     解释：区间范围是：
+     [0,2] --> "0->2"
+     [4,5] --> "4->5"
+     [7,7] --> "7"
+     示例 2：
+
+     输入：nums = [0,2,3,4,6,8,9]
+     输出：["0","2->4","6","8->9"]
+     解释：区间范围是：
+     [0,0] --> "0"
+     [2,4] --> "2->4"
+     [6,6] --> "6"
+     [8,9] --> "8->9"
+     */
+    
+    func summaryRanges(_ nums: [Int]) -> [String] {
+        guard nums.count > 0 else {
+            return []
+        }
+        var startIndex = 0
+        var endIndex = 0
+        let arrowMark = "->"
+        var results = [String]()
+        while startIndex <= endIndex, endIndex < nums.count, startIndex < nums.count {
+            let valStart = nums[startIndex]
+            let valEnd = nums[endIndex]
+            // 判断窗口能否滑动： 1. end+1 < nums.count 2. nums[end + 1] = valEnd + 1
+            guard endIndex + 1 < nums.count, nums[endIndex + 1] == valEnd + 1 else {
+                // 采集结果
+                if startIndex == endIndex {
+                    results.append(String(valStart))
+                } else {
+                    results.append([String(valStart), arrowMark, String(valEnd)].joined())
+                }
+                // 滑动左边界
+                startIndex = endIndex + 1
+                endIndex = startIndex
+                continue
+            }
+            // 可以继续滑动
+            endIndex += 1
+        }
+        return results
+    }
 }
-ArraySolution().maxArea([1,8,6,2,5,4,8,3,7])
+ArraySolution().summaryRanges([0,2,3,4,6,8,9])
