309673fd1d8d739c319273017bfd5542d14af9d3
RavenDeng <dlfkid@icloud.com>
2024-12-03 17:44:55 +0800

    add: [二叉树]637.每层节点的平均值

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -1485,6 +1485,53 @@ class TreeNodeSolution {
             return sumDip(root.left, previousSum: sum) + sumDip(root.right, previousSum: sum)
         }
     }
+    
+    /*
+     637. 二叉树的层平均值
+     给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10-5 以内的答案可以被接受。
+
+      
+
+     示例 1：
+
+
+
+     输入：root = [3,9,20,null,null,15,7]
+     输出：[3.00000,14.50000,11.00000]
+     解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+     因此返回 [3, 14.5, 11] 。
+     示例 2:
+
+
+
+     输入：root = [3,9,20,15,7]
+     输出：[3.00000,14.50000,11.00000]
+
+     */
+    func averageOfLevels(_ root: TreeNode?) -> [Double] {
+        guard let root = root else {
+            return [0]
+        }
+        var results = [Double]()
+        var queue = [TreeNode]()
+        queue.append(root)
+        while !queue.isEmpty {
+            var sum: Double = 0.0
+            let size = queue.count
+            for index in 0 ..< size {
+                let node = queue.removeFirst()
+                sum += Double(node.val)
+                if let leftNode = node.left {
+                    queue.append(leftNode)
+                }
+                if let rightNode = node.right {
+                    queue.append(rightNode)
+                }
+            }
+            results.append(sum / Double(size))
+        }
+        return results
+    }
 }
 
 public class Node {
