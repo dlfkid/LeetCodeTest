dff8d7c3aa327fe8c90a4f7296943267fe66a0cf
RavenDeng <dlfkid@icloud.com>
2024-12-09 15:02:07 +0800

    add: [二叉树]222. 完全二叉树的节点个数

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -1532,6 +1532,49 @@ class TreeNodeSolution {
         }
         return results
     }
+    
+    /*
+     222. 完全二叉树的节点个数
+     给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+
+     完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+
+      
+
+     示例 1：
+
+
+     输入：root = [1,2,3,4,5,6]
+     输出：6
+     示例 2：
+
+     输入：root = []
+     输出：0
+     示例 3：
+
+     输入：root = [1]
+     输出：1
+     */
+    func countNodes(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        var result = 0
+        var queue = [TreeNode]()
+        queue.append(root)
+        while !queue.isEmpty {
+            if let node = queue.popLast() {
+                result += 1
+                if let leftNode = node.left {
+                    queue.append(leftNode)
+                }
+                if let rightNode = node.right {
+                    queue.append(rightNode)
+                }
+            }
+        }
+        return result
+    }
 }
 
 public class Node {
